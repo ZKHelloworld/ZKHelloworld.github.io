5699d605fe78302557cb452c9b0caa4d042ae046
Zheng <zhengkenghong@gmail.com>
2014-04-05 22:48:15 +0800

    Refactor grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,8 @@ module.exports = function(grunt) {
         concat: {
             production: {
                 files: {
-                    'dist/js/lib.min.js': ['js/lib/*.js'],
-                    'dist/ui.js'        : ['js/ui/*.js']
+                    'dist/lib.min.js': ['js/lib/*.js'],
+                    'dist/ui.js'     : ['js/ui/*.js'] //temp js, del by copy
                 }
             }
         },
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
                 options: { compress: true, mongle: true },
                 files: {
                     'dist/js/ui.min.js'  : ['dist/ui.js'],
-                    'dist/js/main.min.js': ['dist/main.js']
+                    'dist/js/main.min.js': ['js/main.js']
                 }
             }
         },
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                 files: [
                     {expand: true, src: ['index.html'], dest: 'dist/'},
                     {expand: true, src: ['html/**'],    dest: 'dist/'},
-                    {expand: true, src: ['image/**'],   dest: 'dist/'}
+                    {expand: true, src: ['image/**'],   dest: 'dist/'},
                 ]
             }
         },
@@ -43,14 +43,19 @@ module.exports = function(grunt) {
                 src: ['dist/*']
             }
         },
+        connect: {
+            server: {
+                options: { hostname: 'localhost', port: 8080 , base: 'dist' }
+            }
+        },
         watch: {
             less: {
                 files: 'less/*.less',
                 tasks: 'less'
             },
             script: {
-                files: 'js/*.js',
-                tasks: ['concat', 'uglify', 'clean']
+                files: 'js/**',
+                tasks: ['concat', 'uglify', 'clean:production']
             },
             html: {
                 files: ['index.html', 'html/*.html', 'image/*'],
@@ -60,13 +65,15 @@ module.exports = function(grunt) {
     });
 
     // 2. Load tasks
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less'  );
-    grunt.loadNpmTasks('grunt-contrib-copy'  );
-    grunt.loadNpmTasks('grunt-contrib-clean' );
-    grunt.loadNpmTasks('grunt-contrib-watch' );
+    grunt.loadNpmTasks('grunt-contrib-concat' );
+    grunt.loadNpmTasks('grunt-contrib-uglify' );
+    grunt.loadNpmTasks('grunt-contrib-less'   );
+    grunt.loadNpmTasks('grunt-contrib-copy'   );
+    grunt.loadNpmTasks('grunt-contrib-clean'  );
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch'  );
 
     // 3. Register tasks
-    grunt.registerTask('default', ['copy', 'concat', 'uglify', 'clean:production', 'less', 'watch']);
+    grunt.registerTask('default',
+                       ['copy', 'concat', 'uglify', 'clean:production', 'less', 'connect', 'watch']);
 };
